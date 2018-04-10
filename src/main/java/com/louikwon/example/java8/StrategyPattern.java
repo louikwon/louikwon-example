3836c52811fec011aa132b0bfe950bea8577d2a1
loui.kwon <loui.kwon@kakaocorp.com>
2018-04-11 00:16:02 +0900

    람다를 활용한 전략패턴 예제

---
+++ src/main/java/com/louikwon/example/java8/StrategyPattern.java
@@ -0,0 +1,31 @@
+package com.louikwon.example.java8;
+
+public class StrategyPattern {
+    public interface InputValidatation {
+        boolean execute(String s);
+    }
+
+    public class IsEmail implements InputValidatation {
+        public boolean execute(String s) {
+            return s.matches("^[_a-z0-9-]+(.[_a-z0-9-]+)*@(?:\\w+\\.)+\\w+$");
+        }
+    }
+
+    public class IsPhoneNumber implements InputValidatation {
+        public boolean execute(String s) {
+            return s.matches("^(01[016789]{1}|02|0[3-9]{1}[0-9]{1})-?[0-9]{3,4}-?[0-9]{4}$");
+        }
+    }
+
+    public class Validator {
+        private final InputValidatation validatation;
+
+        public Validator(InputValidatation validatation) {
+            this.validatation = validatation;
+        }
+
+        public boolean validate(String s) {
+            return validatation.execute(s);
+        }
+    }
+}
---
+++ src/test/java/com/louikwon/example/DesignPatternTest.java
@@ -0,0 +1,49 @@
+package com.louikwon.example;
+
+import com.louikwon.example.java8.StrategyPattern;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class DesignPatternTest {
+    @Test
+    public void 람다사용전_전략패턴_실행예제() {
+        //기존 방식
+        StrategyPattern.IsEmail emailValid = new StrategyPattern().new IsEmail();
+        StrategyPattern.Validator emailValidator = new StrategyPattern().new Validator(emailValid);
+        boolean isEmail = emailValidator.validate("test@daum.net");
+
+        Assert.assertTrue(isEmail);
+
+        StrategyPattern.IsPhoneNumber phoneNumberValid = new StrategyPattern().new IsPhoneNumber();
+        StrategyPattern.Validator phoneNumberValidator = new StrategyPattern().new Validator(phoneNumberValid);
+        boolean isPhone = phoneNumberValidator.validate("02-1111-1111");
+
+        Assert.assertTrue(isPhone);
+    }
+
+    @Test
+    public void 람다사용한_전략패턴() {
+        //람다를 직접 전달하여 새로운 클래스를 구현할 필요가 없다.
+        StrategyPattern.Validator emailValidator = new StrategyPattern().new Validator(
+                (String s) -> s.matches("^[_a-z0-9-]+(.[_a-z0-9-]+)*@(?:\\w+\\.)+\\w+$")
+        );
+        boolean isEmail = emailValidator.validate("test@daum.net");
+
+        Assert.assertTrue(isEmail);
+
+        StrategyPattern.Validator phoneNumberValidator = new StrategyPattern().new Validator(
+                (String s) -> s.matches("^(01[016789]{1}|02|0[3-9]{1}[0-9]{1})-?[0-9]{3,4}-?[0-9]{4}$")
+        );
+        boolean isPhone = phoneNumberValidator.validate("02-1111-1111");
+
+        Assert.assertTrue(isPhone);
+    }
+
+}
