f6ea4100d52b921223e9ff6fa7525328b5138e43
loui.kwon <loui.kwon@kakaocorp.com>
2018-04-27 13:45:05 +0900

    comparing_조합예제 추가

--- src/test/java/com/louikwon/example/LamdaChainTest.java
+++ src/test/java/com/louikwon/example/LamdaChainTest.java
@@ -10,16 +10,37 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.validation.constraints.AssertTrue;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Slf4j
 public class LamdaChainTest {
+    @Test
+    public void comparing_조합예제() {
+        List<Car> carList = new ArrayList();
+        carList.add(
+                Car.builder().name("BMWX6").price(1000).year(2015).build()
+        );
+        carList.add(
+                Car.builder().name("K7").price(600).year(2017).build()
+        );
+        carList.add(
+                Car.builder().name("MINI").price(600).year(2014).build()
+        );
+        carList.add(
+                Car.builder().name("CRUZE").price(300).year(2013).build()
+        );
+
+
+        new LamdaChain().comparatorChain(carList)
+            .orElseGet(Collections::emptyList)
+            .forEach(car -> {
+                log.info(">> name {} / year {} / price {}", car.getName(), car.getYear(), car.getPrice());
+            });
+    }
 
 
     @Test
